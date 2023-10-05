f1ca83ec2f4c9bf7932e4e38477281d3fa5bc90d
PierreChavez <pchavez@vumigroup.com>
2023-10-05 16:51:14 -0500

    resilience4j

--- shopping-service/src/main/java/com/ap/store/shopping/client/CustomerClient.java
+++ shopping-service/src/main/java/com/ap/store/shopping/client/CustomerClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(name = "customer-service", path = "/customers", fallback = CustomerHystrixFallbackFactory.class)
+@FeignClient(name = "customer-service", path = "/customers")
 public interface CustomerClient {
 
     @GetMapping(value = "/{id}")
---
+++ shopping-service/src/main/java/com/ap/store/shopping/client/FeignConfiguration.java
@@ -0,0 +1,34 @@
+package com.ap.store.shopping.client;
+
+import feign.Feign;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.cloud.openfeign.CircuitBreakerNameResolver;
+import org.springframework.cloud.openfeign.FeignFormatterRegistrar;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+
+@Configuration
+public class FeignConfiguration {
+  @Bean
+  public Customizer<Resilience4JCircuitBreakerFactory> circuitBreakerFactoryCustomizer() {
+ CircuitBreakerConfig cbConfig = CircuitBreakerConfig.custom()
+                .slidingWindowType(CircuitBreakerConfig.SlidingWindowType.COUNT_BASED)
+                .slidingWindowSize(5)
+                .failureRateThreshold(20.0f)
+                .waitDurationInOpenState(Duration.ofSeconds(5))
+                .permittedNumberOfCallsInHalfOpenState(5)
+                .build();
+
+                 return resilience4JCircuitBreakerFactory -> resilience4JCircuitBreakerFactory.configure(builder ->
+                builder.circuitBreakerConfig(cbConfig), "CustomerClient#getCustomer(long)");
+  }
+
+    @Bean
+    public CircuitBreakerNameResolver circuitBreakerNameResolver() {
+        return (feignClientName, target, method) -> Feign.configKey(target.type(), method);
+    }
+}
